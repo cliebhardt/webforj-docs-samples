2bd272b3a16502e28349905ed90ade64696e8926
cliebhardt <liebhadt.christian@gmail.com>
2024-07-17 15:58:13 +0200

    Removed not running tests in headless mode

--- src/test/java/fixtures/ChromiumFixtures.java
+++ src/test/java/fixtures/ChromiumFixtures.java
@@ -15,8 +15,7 @@ public class ChromiumFixtures {
   @BeforeAll
   void launchBrowser() {
     playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-      .setHeadless(false));
+    browser = playwright.chromium().launch();
   }
 
   @AfterAll
